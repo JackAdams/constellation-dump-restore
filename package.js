feaf416c8c5fddb11c5302854db0f9b77b8107f4
Brent Abrahams <brent_abrahams@yahoo.com>
2018-02-20 13:21:39 +0800

    Updated dependencies

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'constellation:dump-restore',
-  version: '0.4.6',
+  version: '0.4.7',
   summary: 'Database dump/restore plugin for Constellation',
   git: 'https://github.com/JackAdams/constellation-dump-restore.git',
   documentation: 'README.md',
@@ -11,7 +11,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.0');
 
   api.use(['templating','blaze','underscore','reactive-dict'], 'client');
-  api.use('constellation:console@1.4.6', 'client');
+  api.use('constellation:console@1.4.7', 'client');
   api.use('juliancwirko:s-alert@3.2.0', 'client');
 
   api.addFiles('dump-restore.css', 'client');
